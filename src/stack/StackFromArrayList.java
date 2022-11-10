686435c23a9235133861dfd3f1c0f52284ed69f3
SanjuManna <sanjumanna2201@gmail.com>
2022-11-11 00:22:23 +0530

    Initial Commit

---
+++ src/stack/StackFromArrayList.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.util.*;
+
+public class StackFromArrayList {
+
+    int top;
+    public List<String> list;
+
+    StackFromArrayList(){
+        list = new ArrayList<>();
+        top =0;
+    }
+
+    public void push(int data){
+
+
+    }
+
+    public static void main(String[] args) {
+        StackFromArrayList stk = new StackFromArrayList();
+
+        stk.push(50);
+
+    }
+
+
+}
